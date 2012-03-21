ca363258ace36c607c21f4985fcdfe65c608e51b
nullterminated <ramseygurley@gmail.com>
2012-03-20 20:12:28 -0700

    Initial commit of persistent session storage example apps

---
+++ ModernNoPSS/Sources/wowodc/pssexample/Application.java
@@ -0,0 +1,25 @@
+package wowodc.pssexample;
+
+import er.extensions.appserver.ERXApplication;
+import er.extensions.appserver.navigation.ERXNavigationManager;
+
+public class Application extends ERXApplication {
+	public static void main(String[] argv) {
+		ERXApplication.main(argv, Application.class);
+	}
+
+	public Application() {
+		ERXApplication.log.info("Welcome to " + name() + " !");
+		setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
+		setAllowsConcurrentRequestHandling(true);		
+	}
+	
+    @Override
+    public void finishInitialization() {
+    	super.finishInitialization();
+    	
+    	// Setup main navigation
+    	ERXNavigationManager.manager().configureNavigation();
+    	
+    }
+}
