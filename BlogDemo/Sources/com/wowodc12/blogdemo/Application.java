805441bf70fc325f743ed96f21b1ed0f3234c2a3
Pascal Robert <probert@macti.ca>
2012-06-26 12:47:11 -0400

    Added ERRest to the classpath + registration of request handler

--- BlogDemo/Sources/com/wowodc12/blogdemo/Application.java
+++ BlogDemo/Sources/com/wowodc12/blogdemo/Application.java
@@ -1,6 +1,7 @@
 package com.wowodc12.blogdemo;
 
 import er.extensions.appserver.ERXApplication;
+import er.rest.routes.ERXRouteRequestHandler;
 
 public class Application extends ERXApplication {
 	public static void main(String[] argv) {
@@ -10,6 +11,11 @@ public class Application extends ERXApplication {
 	public Application() {
 		ERXApplication.log.info("Welcome to " + name() + " !");
 		/* ** put your initialization code in here ** */
-		setAllowsConcurrentRequestHandling(true);		
+		setAllowsConcurrentRequestHandling(true);
+		
+		ERXRouteRequestHandler restRequestHandler = new ERXRouteRequestHandler();
+		ERXRouteRequestHandler.register(restRequestHandler);
+		setDefaultRequestHandler(restRequestHandler);
+
 	}
 }
